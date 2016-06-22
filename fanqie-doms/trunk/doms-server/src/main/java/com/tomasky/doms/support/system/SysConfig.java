1139068a9d0ddd877380fc488113381398f4afb3
xiezongming <xiezongming@671eefe9-2466-459f-b5fc-367f35298b91>
2016-06-22 01:29:49 +0000

    ProvidToQunarApi

--- fanqie-doms/trunk/doms-server/src/main/java/com/tomasky/doms/support/system/SysConfig.java
+++ fanqie-doms/trunk/doms-server/src/main/java/com/tomasky/doms/support/system/SysConfig.java
@@ -11,11 +11,20 @@ import org.springframework.stereotype.Component;
  * DOTO: 系统常用配置
  */
 @Component
-public class SysConfig implements InitializingBean {
+public class SysConfig {
     public static String domainOms;//oms
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        domainOms = PropertiesUtil.readFile("/config.properties", "domain_oms");
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        domainOms = PropertiesUtil.readFile("/config.properties", "domain_oms");
+//    }
+
+
+    public static String getDomainOms() {
+        return domainOms;
+    }
+
+    public static void setDomainOms(String domainOms) {
+        SysConfig.domainOms = domainOms;
     }
 }
