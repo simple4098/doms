59971323765401089ee30857fafd812de98ab0dd
xiezongming <xiezongming@671eefe9-2466-459f-b5fc-367f35298b91>
2016-07-04 10:04:03 +0000

    ProvidToQunarApi

--- doms-server/src/main/java/com/tomasky/doms/support/util/CommonUtil.java
+++ doms-server/src/main/java/com/tomasky/doms/support/util/CommonUtil.java
@@ -10,8 +10,6 @@ import java.io.*;
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class CommonUtil {
     private final static Logger logger = LoggerFactory.getLogger(CommonUtil.class);
@@ -167,14 +165,19 @@ public class CommonUtil {
      * @return
      */
     public static boolean isDate(String date) {
-        /**
-         * 判断日期格式和范围
-         */
-        String rexp = "^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))";
-        Pattern pat = Pattern.compile(rexp);
-        Matcher mat = pat.matcher(date);
-        boolean dateType = mat.matches();
-        return dateType;
+        if (!StringUtils.isEmpty(date)) {
+            String newDate = date.replaceAll("/", "-");
+            try {
+                Date re = JodaTimeUtil.parse(newDate);
+                if (re != null) {
+                    return true;
+                }
+            } catch (Exception e) {
+                logger.error("不是有效的日期格式："+date);
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 
     /**
